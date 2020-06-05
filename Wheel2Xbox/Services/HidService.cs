0fea76cc2a732dd577bd8a7da7cace59d896d19d
Polarts <polarbear.oc@gmail.com>
2020-06-05 13:12:56 +0300

    Init commit

---
+++ Wheel2Xbox/Services/HidService.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using HidLibrary;
+
+namespace Wheel2Xbox.Services
+{
+    public delegate void InputReportChangeEventHandler(IEnumerable<int> changes);
+
+    public class HidService
+    {
+        #region Fields
+
+        HidDevice device;
+
+        /// <summary>
+        /// A timer that reads data from the HID device once elapsed.
+        /// </summary>
+        Timer readTimer;
+
+        /// <summary>
+        /// Indicates whether the service has already been created.
+        /// </summary>
+        static bool isCreated = false;
+
+        /// <summary>
+        /// Caching the last report read from the device for comparison, as it is required in change detection.
+        /// </summary>
+        byte[] lastReport;
+
+        #endregion
+
+        #region Events
+
+        /// <summary>
+        /// Fires when an input has been successfully received.<br></br>
+        /// Args: an array of changes represented as int values.
+        /// </summary>
+        public event InputReportChangeEventHandler InputReceived;
+
+        #endregion
+
+        #region Methods
+
+        #region Ctor and Factory
+
+        public static HidService Create(ushort vendorId = 0x046D, ushort productId = 0xCA04)
+        {
+            if (isCreated)
+                throw new InvalidOperationException("You cannot create more than one HidService");
+
+            return new HidService(vendorId, productId);
+        }
+
+        private HidService(ushort vendorId = 0x046D, ushort productId = 0xCA04)
+        {
+            device = HidDevices.Enumerate(vendorId, productId).ToArray()[0];
+            readTimer = new Timer(onReadTimerElapsed, null, 0, 1000 / 60);
+        }
+
+        #endregion
+
+        private  void onReadTimerElapsed(object state)
+        {
+            var input = device.Read();
+            if (input.Status == HidDeviceData.ReadStatus.Success
+                && input.Data.Length > 0)
+            {
+                if (lastReport is null)
+                    lastReport = input.Data;
+                else
+                {
+                    var changes = input.Data.Zip(lastReport, subtractBytesAsInt);
+                    InputReceived?.Invoke(changes);
+                }
+            }
+
+        }
+
+        private  int subtractBytesAsInt(byte current, byte last) => current - last;
+
+        #endregion
+    }
+}
---
+++ Wheel2Xbox/Services/ScpX360Service.cs
@@ -0,0 +1,40 @@
+﻿using ScpDriverInterface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Wheel2Xbox.Services
+{
+    public class ScpX360Service
+    {
+        #region Fields
+
+        ScpBus bus;
+
+        X360Controller controller;
+
+        static bool isCreated = false;
+
+        #endregion
+
+        #region Methods
+
+        public static ScpX360Service Create()
+        {
+            if (isCreated)
+                throw new InvalidOperationException("You cannot create more than one ScpX360Service");
+
+            return new ScpX360Service();
+        }
+
+        private ScpX360Service()
+        {
+            bus = new ScpBus();
+            controller = new X360Controller();
+
+            bus.PlugIn(1);
+        }
+
+        #endregion
+    }
+}
