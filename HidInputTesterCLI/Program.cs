29fad1a766da5377dc8f60b5a02d5a4b85596de1
Polarts <polarbear.oc@gmail.com>
2020-06-05 16:29:23 +0300

    Added CLI project to test input reports

---
+++ HidInputTesterCLI/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using HidLibrary;
+
+namespace HidInputTesterCLI
+{
+    class Program
+    {
+        static HidDevice device;
+
+        static void Main(string[] args)
+        {
+            device = HidDevices.Enumerate(0x046D, 0xCA04).ToArray()[0];
+            var readTimer = new Timer(onReadTimerElapsed, null, 0, 1000 / 60);
+            Console.ReadLine();
+        }
+
+        static byte[] lastReport;
+        private static void onReadTimerElapsed(object state)
+        {
+            var input = device.Read();
+            if (input.Status == HidDeviceData.ReadStatus.Success
+                && input.Data.Length > 0)
+            {
+                Console.WriteLine("Report: "+string.Join(' ', input.Data));
+
+                if (lastReport is null)
+                    lastReport = input.Data;
+                else
+                {
+                    Console.WriteLine("Changes: " + string.Join(' ', input.Data.Zip(lastReport, (current, last) => current - last)));
+                    lastReport = input.Data;
+                }
+
+            }
+
+        }
+    }
+}
