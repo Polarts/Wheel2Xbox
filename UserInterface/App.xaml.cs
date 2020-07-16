3f50d3826bb919997b64bc5af229c38d88e2e22c
Polarts <polarbear.oc@gmail.com>
2020-07-16 22:17:24 +0300

    Moved system tray icon to WPF project

---
+++ UserInterface/App.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using Wheel2Xbox;
+
+namespace UserInterface
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+
+        public static WheelIOManager WheelIOManager;
+
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            //WheelIOManager = WheelIOManager.Create();
+        }
+    }
+}
